afd73d4235cdc5071597af3bdeeb7085123b4296
Andy Davidson <aedavids@ucsc.edu>
2021-04-01 09:29:09 -0700

    script to download fastq and annotations

---
+++ problemSet1/bin/downloadData.sh
@@ -0,0 +1,5 @@
+set -x
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_37/gencode.v37.annotation.gtf.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_37/GRCh38.p13.genome.fa.gz
+wget https://www.encodeproject.org/files/ENCFF000IJA/@@download/ENCFF000IJA.fastq.gz
+wget https://www.encodeproject.org/files/ENCFF000IJH/@@download/ENCFF000IJH.fastq.gz
